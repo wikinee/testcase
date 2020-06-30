bb68421a4a2b6a55e87917b202461a3c6fd5ba45
wikinee <neeyongliang@gmail.com>
2020-06-30 14:40:43 +0800

    PythonTest: update test-code-count.py

--- PythonTest/test-code-count.py
+++ PythonTest/test-code-count.py
@@ -15,17 +15,17 @@
 """
 
 import os
+import time
+import sys
 import subprocess
 import openpyxl
-import sys
-import time
 
-SOURCE_CODE_PATH="/home/someone/CodeCount/Source"
-UNPACK_CODE_PATH="/home/someone/CodeCount/Unpack"
-XLSX_FILE="/home/someone/CodeCount/test.xlsx"
-LOG_FILE="/home/someone/CodeCount/test.log"
-CLOC_TIMEOUT=300
-BLACK_LIST=[
+SOURCE_CODE_PATH = "/home/someone/CodeCount/Source"
+UNPACK_CODE_PATH = "/home/someone/CodeCount/Unpack"
+XLSX_FILE = "/home/someone/CodeCount/test.xlsx"
+LOG_FILE = "/home/someone/CodeCount/test.log"
+CLOC_TIMEOUT = 300
+BLACK_LIST= [
     'thunderbird-52.2.1',
     'libreoffice-5.2.7',
     'firefox-esr-45.9.0esr',
@@ -39,7 +39,7 @@ class TestCodeCount(object):
     说明：
         TestCodeCount 是专门为源码统计而编写的脚本, 使用本脚本之前，要在系统中安装 cloc 和 dpkg-dev 包。
         其中 cloc 统计代码数量， dpkg-dev 中 dpkg-source -x 用于解压源码包。
-        
+
     日志：
         black_list=>源码文件过大，跳过该目录；
         list index out of range=>调用 cloc 没有生成 SUM: 结果，运行 grep 失败造成的，统计无效；
@@ -55,7 +55,7 @@ class TestCodeCount(object):
         20200630 add from_index and to_index feature, or pass less than 0 to skip it
     """
 
-    def __init__ (self):
+    def __init__(self):
         """初始化"""
         self.cloc_cmd = "cloc --autoconf --by-file --exclude-dir .pc "
         self.failed_list = []
@@ -128,51 +128,52 @@ class TestCodeCount(object):
         """ 构建解压目录 """
         if os.path.isdir(UNPACK_CODE_PATH):
             return
-        elif not os.path.exists(UNPACK_CODE_PATH):
+        if not os.path.exists(UNPACK_CODE_PATH):
             os.mkdir(UNPACK_CODE_PATH)
         else:
             print("{} is not directory, exit".format(UNPACK_CODE_PATH))
             sys.exit(1)
 
-    def subprocess_run_command(self, cmd, success_message, output_file):
+    def subprocess_run_command(self, cmd, success_message, filename):
         """ 使用 subprocess 运行命令
 
         :param cmd: 运行的 shell 命令
         :param success_message: 成功时的输出信息
-        :param output_file: 是否要输出到文件
+        :param filename: 是否要输出到文件
         """
-        p = None
+        sub_proc = None
         try:
-            if output_file is None:
-                p = subprocess.Popen(cmd, shell=True)
-                p.wait()
+            if filename is None:
+                sub_proc = subprocess.Popen(cmd, shell=True)
+                sub_proc.wait()
             else:
-                p = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-                # print(p.stdout)
-                with open(output_file, 'wb') as f:
-                    f.write(p.stdout)
-        except subprocess.TimeoutExpired as e:
+                sub_proc = subprocess.run(cmd, shell=True,
+                                          stdout=subprocess.PIPE,
+                                          stderr=subprocess.PIPE)
+                with open(filename, 'ab+') as output_file:
+                    output_file.write(sub_proc.stdout)
+        except subprocess.TimeoutExpired as err:
             self.failed_list.append("subprocess command failed for: " + cmd)
-            print(e)
-        except subprocess.SubprocessError as e:
+            print(err)
+        except subprocess.SubprocessError as err:
             self.failed_list.append("subprocess command failed for: " + cmd)
-            print(e)
-        except Exception as e:
+            print(err)
+        except Exception as err:
             self.failed_list.append("subprocess command failed for: " + cmd)
-            print(e)
+            print(err)
         else:
-            if not p and ('0' != p.returncode):
+            if not sub_proc and (sub_proc.returncode != '\0'):
                 self.failed_list.append("subprocess returncode not 0: " + cmd)
             else:
                 print(success_message)
 
-    def unpack_source_packages(self, source_dir, unpack_dir, from_index, to_index):
+    def unpack_source_packages(self, source_dir, unpack_dir, start_index, end_index):
         """解压源码包
 
         :param source_dir: 源码包存放的路径
         :param unpack_dir: 解压源码的路径
-        :param from_index: 开始索引
-        :param to_index: 结束索引
+        :param start_index: 开始索引
+        :param end_index: 结束索引
         :return:
         """
 
@@ -181,30 +182,30 @@ class TestCodeCount(object):
             print("No package need to unpack!")
             return
 
-        if from_index > to_index:
+        if start_index > end_index:
             self.failed_list.append("unpack source package index error")
 
         unpack_index = 0
         dsc_index = 0
 
         dsc_files.sort()
-        with open(LOG_FILE, 'a+') as f:
-            f.write("dsc files list:\n")
+        with open(LOG_FILE, 'a+') as log_file:
+            log_file.write("dsc files list:\n")
             for dsc in dsc_files:
                 dsc_index = dsc_index + 1
                 print("dsc file: " + os.path.join(unpack_dir, dsc))
-                f.write(os.path.join(unpack_dir, dsc) + '\n')
-                if from_index > 0 and dsc_index < from_index:
+                log_file.write(os.path.join(unpack_dir, dsc) + '\n')
+                if start_index > 0 and dsc_index < start_index:
                     continue
-                if to_index > 0 and dsc_index > to_index:
+                if dsc_index > end_index > 0:
                     break
-            f.write("===========================\n")
+            log_file.write("===========================\n")
 
         for dsc in dsc_files:
             unpack_index = unpack_index + 1
-            if from_index > 0 and unpack_index < from_index:
+            if start_index > 0 and unpack_index < start_index:
                 continue
-            if to_index > 0 and unpack_index > to_index:
+            if unpack_index > end_index > 0:
                 break
             cmd = 'cd ' + unpack_dir + ';dpkg-source -x ' + dsc
             # print(cmd)
@@ -215,12 +216,12 @@ class TestCodeCount(object):
         if not self.failed_list:
             return True
 
-        with open(LOG_FILE, 'a+') as f:
-            f.write(failed_reason)
+        with open(LOG_FILE, 'a+') as log_file:
+            log_file.write(failed_reason)
             for item in self.failed_list:
                 print("Error: " + item)
-                f.write(item + '\n')
-            f.write("===========================\n")
+                log_file.write(item + '\n')
+            log_file.write("===========================\n")
         return False
 
     def clear_none_directory(self, unpack_dir):
@@ -245,23 +246,24 @@ class TestCodeCount(object):
             code = int(outputs[3])
             code_sum = blank + commend + code
             return code_sum
-        else:
-            return 0
+        return 0
 
-    def generate_xlsx(self, unpack_codes, from_index, to_index):
-        if from_index >= to_index:
+    def generate_xlsx(self, unpack_codes, start_index, end_index):
+        """ 生成 xlsx 文件 """
+        codes_nums = len(unpack_codes)
+        if codes_nums == 0:
+            return
+        if start_index >= end_index:
             self.failed_list.append("generate xlsx index error")
             return
         xlsx_nodes = []
         index = 0
-        codes_nums = len(unpack_codes)
-        if codes_nums != 0:
-            unpack_codes.sort()
+        unpack_codes.sort()
         for code_dir in unpack_codes:
             index = index + 1
-            if from_index > 0 and index < from_index:
+            if start_index > 0 and index < start_index:
                 continue
-            if to_index > 0 and index > to_index:
+            if index > end_index > 0:
                 break
             print("generate {}/{} node for {}".format(index, codes_nums, code_dir))
             package_name = "NoName"
@@ -286,8 +288,7 @@ class TestCodeCount(object):
             if not os.path.exists(control_file):
                 self.failed_list.append("get control file failed: " + control_file)
                 continue
-            try:
-                file = open(control_file, 'r+')
+            with open(control_file, 'r+') as file:
                 for line in file.readlines():
                     line = line.strip('\n')
                     if line.startswith("Source:"):
@@ -304,36 +305,36 @@ class TestCodeCount(object):
 
                     if line.startswith("Description:"):
                         break
-            except:
-                self.failed_list.append("parse control file failed: " + code_dir)
-            finally:
-                file.close()
-            xlsx_nodes.append([package_name, cloc_sum, package_maintainer, package_homepage, package_description])
+            xlsx_nodes.append([package_name,
+                               cloc_sum,
+                               package_maintainer,
+                               package_homepage,
+                               package_description])
 
         if not os.path.exists(XLSX_FILE):
-            wb = openpyxl.Workbook()
-            sheetnames = wb.get_sheet_names()
-            ws = wb.get_sheet_by_name(sheetnames[0])
-            ws['A1'] = "Package Name"
-            ws['B1'] = "Cloc"
-            ws['C1'] = "Package Maintainer"
-            ws['D1'] = "Package Homepace"
-            ws['E1'] = "Package Description"
+            data = openpyxl.Workbook()
+            sheetnames = data.get_sheet_names()
+            sheet = data.get_sheet_by_name(sheetnames[0])
+            sheet['A1'] = "Package Name"
+            sheet['B1'] = "Cloc"
+            sheet['C1'] = "Package Maintainer"
+            sheet['D1'] = "Package Homepace"
+            sheet['E1'] = "Package Description"
         else:
-        	# 追加写入
-            wb = openpyxl.load_workbook(XLSX_FILE)
-            ws = wb.active
+            # 追加写入
+            data = openpyxl.load_workbook(XLSX_FILE)
+            sheet = data.active
         for node in xlsx_nodes:
-            ws.append(node)
-        wb.save(filename=XLSX_FILE)
+            sheet.append(node)
+        data.save(filename=XLSX_FILE)
 
-    def start_cloc(self, option, from_index=-1, to_index=-1):
+    def start_cloc(self, option="NoOption", start_index=-1, end_index=-1):
         print("%s start ..." % option)
         self.failed_list = []
         if option == "unpack":
             # unpack
             self.build_workspace()
-            self.unpack_source_packages(SOURCE_CODE_PATH, UNPACK_CODE_PATH, from_index, to_index)
+            self.unpack_source_packages(SOURCE_CODE_PATH, UNPACK_CODE_PATH, start_index, end_index)
             self.clear_none_directory(UNPACK_CODE_PATH)
             res = self.print_failed_list("unpack failed list:\n")
             if not res:
@@ -341,7 +342,7 @@ class TestCodeCount(object):
         elif option == "calcu":
             # calculate
             unpack_codes = self.get_code_dirs(UNPACK_CODE_PATH)
-            self.generate_xlsx(unpack_codes, from_index, to_index)
+            self.generate_xlsx(unpack_codes, start_index, end_index)
             res = self.print_failed_list("generate xlsx failed list:\n")
             if not res:
                 return
@@ -352,18 +353,18 @@ class TestCodeCount(object):
 
     def print_help(self):
         print("Usage:")
-        print("./test-code-count.py option [from_index to_index]")
+        print("code-count.py option [from_index to_index]")
         print("option can be unpack or calcu")
 
 if __name__ == '__main__':
     # 参数模式
-    ncc = TestCodeCount()
+    cc = TestCodeCount()
     argc = len(sys.argv)
 
     if argc != 2 and argc != 4:
-        ncc.print_help()
+        cc.print_help()
         sys.exit(1)
-    option = sys.argv[1]
+    option_type = sys.argv[1]
     from_index = -1
     to_index = -1
     if argc == 4:
@@ -374,20 +375,21 @@ if __name__ == '__main__':
             print("Warning: index argument error, use -1 as default")
             from_index = -1
             to_index = -1
-    ncc.start_cloc(option, from_index, to_index)
+    cc.start_cloc(option_type, from_index, to_index)
 
     # =======================================
 
     # 手动模式
-    # ncc.start_cloc("unpack", 1, 10)
-    # ncc.start_cloc("calcu", 1, 10)
+    # cc.start_cloc("unpack", 1, 10)
     # time.sleep(5)
-    # ncc.start_cloc("calcu", 16, 30)
+    # cc.start_cloc("calcu", 1, 10)
     # time.sleep(5)
-    # ncc.start_cloc("calcu", 31, 45)
+    # cc.start_cloc("calcu", 16, 30)
     # time.sleep(5)
-    # ncc.start_cloc("calcu", 46, 60)
+    # cc.start_cloc("calcu", 31, 45)
     # time.sleep(5)
-    # ncc.start_cloc("calcu", 801, 1000)
+    # cc.start_cloc("calcu", 46, 60)
     # time.sleep(5)
-    # ncc.start_cloc("calcu", 1001, 1200)
+    # cc.start_cloc("calcu", 801, 1000)
+    # time.sleep(5)
+    # cc.start_cloc("calcu", 1001, 1200)
