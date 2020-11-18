8d287b02e957f05705fb8e5a27b4fe9b4aae423e
wikinee <neeyongliang@gmail.com>
2020-11-18 14:09:36 +0800

    CTest: rename tree view testcase

--- CTest/test-json.c
+++ CTest/test-json.c
@@ -3,33 +3,46 @@
 #include<glib.h>
 #include<json-glib/json-glib.h>
 
-
 #define USING_UKEY_LOGIN "/tmp/TESTKEYFILE"
 
-int main() {
+const gchar*
+test_json_parse (const gchar* keyname) {
     GError *error = NULL;
-    const gchar *val;
-    gchar *keyname = "key";
-    JsonParser *ukey_parser = json_parser_new ();
-    gboolean load_res = json_parser_load_from_file (ukey_parser, USING_UKEY_LOGIN, &error);
+    gchar *val;
+    JsonParser *ukey_parser;
+    gboolean load_res;
+    JsonNode *root;
+    JsonObject *root_obj;
+
+    ukey_parser = json_parser_new ();
+    load_res = json_parser_load_from_file (ukey_parser, USING_UKEY_LOGIN, &error);
     if (error) {
         g_print ("parseKeyFile: json parser load file error");
         g_error_free (error);
-        return 1;
+        return NULL;
     }
 
-    JsonNode *root = json_parser_get_root (ukey_parser);
-
+    root = json_parser_get_root (ukey_parser);
     if (root) {
-        JsonObject *root_obj = json_node_get_object (root);
+        root_obj = json_node_get_object (root);
         if (root_obj) {
             JsonNode *account = json_object_get_member (root_obj, keyname);
             if (account) {
-                val = json_node_get_string (account);
+                val = (gchar *)json_node_get_string (account);
+                return (const gchar *)val;
             }
         }
     }
     g_print ("parse key file success, val: %s", val);
 
-    return 2;
+    return NULL;
+}
+
+int main() {
+    const gchar* test_key = "key";
+    const gchar* test_value;
+    test_value = test_json_parse (test_key);
+    g_print ("file: %s, test res: %s\n", __FILE__, test_value);
+
+    return 0;
 }
--- CTest/test-time.c
+++ CTest/test-time.c
@@ -6,7 +6,7 @@
 void get_time_now () {
     GDateTime *utc = g_date_time_new_now_local ();
     gint m = g_date_time_get_microsecond (utc);
-    g_print ("microsecond:%s.%d\n", g_date_time_format (utc, "%X"), m);
+    g_print ("microsecond:%s.%d\n", g_date_time_format (utc, "%Y-%m-%d %H:%M:%S"), m);
 }
 
 int main() {
